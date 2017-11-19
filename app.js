7906230349a92966179f44b97e76fb4b12cf0f91
Victor Salcedo <victorsalc2@gmail.com>
2017-11-19 00:30:52 -0500

    creating app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+var evendays, oneday;
+
+
+evendays = $('.odday');
+oneday = evendays.children();
+oneday.css('font-size','50px');
+
+
+//function to load as page loads
+
+// function someFunction() {
+//     var image, newImage;
+
+//     image = $('img');
+//     newImage = image.attr('src','http://placekitten.com/350/150')
+
+// }
+// $(someFunction)
+
