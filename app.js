381e56a4746a848d9530e2ae415875482399b6be
Victor Salcedo <victorsalc2@gmail.com>
2017-12-09 20:31:37 -0500

    style: edit css and app.js

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+var mustdo = document.querySelector('.mustdo_title');
+
 function menuDisplay() {
 	document.querySelector('.mustdo_recipe').style.display = 'block';
+	mustdo.innerHTML = 'Yummmmmm!';
+
 }
