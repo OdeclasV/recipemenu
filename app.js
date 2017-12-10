6224e8e81846bb1a8184590a309badb99f4cf9a1
Victor Salcedo <victorsalc2@gmail.com>
2017-12-09 20:00:58 -0500

    refractor: edit text in HTML, edit CSS selectors, add menuDisplay() function.

--- app.js
+++ app.js
@@ -1,19 +1,3 @@
-var evendays, oneday;
-
-
-evendays = $('.odday');
-oneday = evendays.children();
-oneday.css('font-size','50px');
-
-
-//function to load as page loads
-
-// function someFunction() {
-//     var image, newImage;
-
-//     image = $('img');
-//     newImage = image.attr('src','http://placekitten.com/350/150')
-
-// }
-// $(someFunction)
-
+function menuDisplay() {
+	document.querySelector('.mustdo_recipe').style.display = 'block';
+}
